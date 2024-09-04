518b6a21bcd82cdc400f05a2b1e95d73190ab2cb
nunoplima <nunoplima@hotmail.com>
2024-09-04 15:57:41 +0100

    add Photo component

---
+++ src/components/Photo/Photo.types.ts
@@ -0,0 +1,7 @@
+import { PropsWithChildren } from 'react'
+
+interface IImage extends PropsWithChildren {
+  imageSrc?: string
+}
+
+export type { IImage }
---
+++ src/components/Photo/index.ts
@@ -0,0 +1,7 @@
+export {
+  PhotoActionButton,
+  PhotoActions,
+  PhotoImage,
+  PhotoLabel,
+  Photo as default,
+} from './Photo'
