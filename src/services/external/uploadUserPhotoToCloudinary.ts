4fa47095c38761edbcd03b7e2b8f1862088d1a26
nunoplima <nunoplima@hotmail.com>
2024-09-10 11:37:04 +0100

    update manifest.json

--- src/services/external/uploadUserPhotoToCloudinary.ts
+++ src/services/external/uploadUserPhotoToCloudinary.ts
@@ -1,3 +1,5 @@
+import ky from 'ky'
+
 interface IUploadUserImageToCloudinaryResponse {
   error?: string
   data?: string
@@ -8,38 +10,38 @@ interface IUploadUserImageToCloudinaryRawResponse {
 }
 
 export const uploadUserPhotoToCloudinary = async (file: File) => {
-  return new Promise((resolve) => {
-    setTimeout(
-      () =>
-        resolve({
-          data: 'https://res.cloudinary.com/nungon/image/upload/v1725531590/jyy0djqsibicvbgrwmlr.webp',
-        }),
-      1000,
-    )
-  }) as Promise<IUploadUserImageToCloudinaryResponse>
+  // return new Promise((resolve) => {
+  //   setTimeout(
+  //     () =>
+  //       resolve({
+  //         data: 'https://res.cloudinary.com/nungon/image/upload/v1725531590/jyy0djqsibicvbgrwmlr.webp',
+  //       }),
+  //     1000,
+  //   )
+  // }) as Promise<IUploadUserImageToCloudinaryResponse>
 
-  // const formData = new FormData()
-  // formData.append('file', file)
-  // formData.append(
-  //   'upload_preset',
-  //   import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET,
-  // )
+  const formData = new FormData()
+  formData.append('file', file)
+  formData.append(
+    'upload_preset',
+    import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET,
+  )
 
-  // try {
-  //   const response: IUploadUserImageToCloudinaryRawResponse = await ky
-  //     .post(
-  //       `${import.meta.env.VITE_CLOUDINARY_BASE_URL}/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/upload`,
-  //       {
-  //         body: formData,
-  //       },
-  //     )
-  //     .json()
+  try {
+    const response: IUploadUserImageToCloudinaryRawResponse = await ky
+      .post(
+        `${import.meta.env.VITE_CLOUDINARY_BASE_URL}/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/upload`,
+        {
+          body: formData,
+        },
+      )
+      .json()
 
-  //   return { data: response.secure_url } as IUploadUserImageToCloudinaryResponse
-  // } catch (error) {
-  //   const errorMessage =
-  //     error instanceof Error ? error.message : 'An unexpected error occurred'
+    return { data: response.secure_url } as IUploadUserImageToCloudinaryResponse
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : 'An unexpected error occurred'
 
-  //   return { error: errorMessage } as IUploadUserImageToCloudinaryResponse
-  // }
+    return { error: errorMessage } as IUploadUserImageToCloudinaryResponse
+  }
 }
